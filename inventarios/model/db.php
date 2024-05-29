47a79ebd0161c0c2321f450c5903da685c9d7932
BokisAxis <117787944+BokisAxis@users.noreply.github.com>
2024-05-29 10:03:17 -0600

    Update db.php

--- inventarios/model/db.php
+++ inventarios/model/db.php
@@ -4,10 +4,10 @@
 //A connection is established to a MySQL database
 $conn = mysqli_connect(  
 
-'localhost',  //MySQL server name
-'u757606859_invetarios',  //MySQL username
-'c+22+Z;fST',  //Mysql password
-'u757606859_inventarios'  //Database name
+'',  //MySQL server name
+'',  //MySQL username
+'',  //Mysql password
+''  //Database name
 );
 
 ?>
